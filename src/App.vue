ed9e65674baeb7413903e2507d09f4d9efec9ade
jezeus17 <116925902+jezeus17@users.noreply.github.com>
2023-10-20 19:58:56 -0400

    feat: base interface

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-
+import BookShelf from './components/BookShelf.vue';
+import Bookcard from './components/BookCard.vue';
 </script>
 
 <template>
-  <header>
- 
-  </header>
+    <h1 id ="title">Select a book </h1>
+    <div id="main">
+            <BookShelf></BookShelf>
+            <Bookcard></Bookcard>
+    </div>
+    
 </template>
 
 <style scoped>
+    #main{
+        display: flex;
+    }
+    #title{
+        padding-left: 2%;
+        user-select: none;
+    }
 
 </style>
