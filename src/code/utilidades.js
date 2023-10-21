30d8c7bc20f34c9531513c9f881d53545496b60d
Acce117 <ernestoalejandrocarraleroconde@gmail.com>
2023-10-21 19:52:22 -0400

    generarIdUnico fixed

--- src/code/utilidades.js
+++ src/code/utilidades.js
@@ -1,6 +1,6 @@
 export const validarNoNullUndefined = (x) => x !== null && x !== undefined
 
-export const generarIdUnico = async () =>
+export const generarIdUnico = () =>
   Math.floor(performance.timeOrigin * performance.now() + performance.timeOrigin)
     .toString(36)
     .substring(0, 9)
