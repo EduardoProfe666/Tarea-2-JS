ab0e0f4c18a6562ecc5a598d1197cf4b4c562d11
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2023-10-28 23:22:21 -0400

    Delete src directory

--- src/code/utilidades.js
+++
@@ -1,6 +0,0 @@
-export const validarNoNullUndefined = (x) => x !== null && x !== undefined
-
-export const generarIdUnico = () =>
-  Math.floor(performance.timeOrigin * performance.now() + performance.timeOrigin)
-    .toString(36)
-    .substring(0, 9)
